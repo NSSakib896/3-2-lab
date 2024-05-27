3ff2bf8af7fd5d6903ccce32ad927c19384ba7cb
sakib <nazmus896@gmail.com>
2024-05-27 13:31:03 +0600

    starting microprocessor 1st code is input output

--- web/project/members/models.py
+++ web/project/members/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Member(models.Model):
     fname = models.CharField(max_length=100)
     lname = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=11,null=True)
+    
+    def __str__(self):
+        return self.fname + " " + self.lname
--- web/project/members/views.py
+++ web/project/members/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
-# Create your views here.
+from .models import Member
+# def members(request):
+#     template = loader.get_template('myfirst.html')
+#     return HttpResponse(template.render())
 
+
+def members(request):
+    text = "Add"
+    template = loader.get_template('all_members.html')
+    member_list = Member.objects.all().values()
+    context = {
+        'member_list': member_list,
+        "button_text": text,
+    }
+    return HttpResponse(template.render(context, request))
