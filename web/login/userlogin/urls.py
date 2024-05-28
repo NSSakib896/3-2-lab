5d50be617964f627bdc7e0bbb214da6f04af67c7
sakib <nazmus896@gmail.com>
2024-05-28 12:38:33 +0600

    if wrong come here

--- web/login/userlogin/urls.py
+++ web/login/userlogin/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', views.index, name='home'),
     path('login/', views.user_login, name='login'),
     path('signup/', views.user_signup, name='signup'),
